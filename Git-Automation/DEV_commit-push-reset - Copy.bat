3f4dd742ef8ec5a2a1b3a9c7e99ddf44bfb94857
Andrii Krasovskyi <andriikr@compatibl.com>
2024-05-16 10:51:57 +0200

    Git Automation

--- Git-Automation/DEV_commit-push-reset - Copy.bat
+++ Git-Automation/DEV_commit-push-reset - Copy.bat
@@ -2,10 +2,14 @@
 REM Change to the directory where your Git repository is located
 cd C:\krass\personal\AHK
 
-
+REM add files 
 git add .
+
 REM Perform git commit with the message "upd BS"
 git commit -m "upd BS"
 
 REM Push the commit to the remote repository
 git push -f
+
+REM Reset the last commit, keeping the changes in the working directory
+git reset --soft HEAD~1
---
+++ Git-Automation/DEV_update.bat
@@ -0,0 +1,8 @@
+@echo off
+REM Change to the directory where your Git repository is located
+cd C:\krass\personal\AHK
+
+ 
+git pull
+
+pause
